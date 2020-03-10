c7bed5c968306783fd268abef56e58edace54028
Nixoor <l2extremel2@gmail.com>
2020-03-10 12:49:51 +0100

    JS 2020-03-10

---
+++ scripts/20203010.R
@@ -0,0 +1,35 @@
+#Włączenie biblioteki
+library(tm)
+
+#zmiana katalogu roboczego
+workDir <- "D:\\JS\\TextMining\\"
+setwd(workDir)
+
+#definicja katalogów funkcjonalnych
+inputDir <- ".\\data"
+outputDir <- ".\\results"
+scriptsDir <- ".\\scripts"
+workspaceDir <- ".\\workspace"
+dir.create(outputDir, showWarnings = FALSE)
+
+#utworzenie korpusu dokumentów
+corpusDir <- paste(inputDir,"Literatura - streszczenia - oryginał", sep="\\")
+corpus <- VCorpus(
+  DirSource(corpusDir,pattern = "*.txt",encoding = "UTF-8"),
+  readerControl = list(langu = "pl_PL")
+)
+
+#wstępne przetwarzanie
+corpus <-tm_map(corpus, removeNumbers)
+corpus <-tm_map(corpus, removePunctuation)
+corpus <-tm_map(corpus, content_transformer(tolower))
+stoplistFile <- paste(inputDir,"\\","stopwords_pl.txt", sep="")
+stoplist <- readLines(
+  stoplistFile,
+  encoding = "UTF-8"
+)
+corpus <-tm_map(corpus, removeWords, stoplist)
+corpus <-tm_map(corpus, stripWhitespace)
+
+writeLines(as.character(corpus[1]))
+writeLine(corpus[1]$content)
