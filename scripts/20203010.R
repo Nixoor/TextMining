f0e5943ffd92dc868bb259d0343d42930a4715d2
Jarosław Starmach <32633257+Nixoor@users.noreply.github.com>
2020-03-24 12:53:24 +0100

    24.03.2020
    24.03.2020

--- scripts/20203010.R
+++ scripts/20203010.R
@@ -1,8 +1,10 @@
 #Włączenie biblioteki
 library(tm)
+library(hunspell)
+library(stringr)
 
 #zmiana katalogu roboczego
-workDir <- "D:\\JS\\TextMining\\"
+workDir <- "C:\\Users\\JS\\Documents\\GitHub\\TextMining\\"
 setwd(workDir)
 
 #definicja katalogów funkcjonalnych
@@ -13,7 +15,7 @@ workspaceDir <- ".\\workspace"
 dir.create(outputDir, showWarnings = FALSE)
 
 #utworzenie korpusu dokumentów
-corpusDir <- paste(inputDir,"Literatura - streszczenia - oryginał", sep="\\")
+corpusDir <- paste(inputDir,"Literatura - streszczenia - orygina�", sep="\\")
 corpus <- VCorpus(
   DirSource(corpusDir,pattern = "*.txt",encoding = "UTF-8"),
   readerControl = list(langu = "pl_PL")
@@ -31,5 +33,39 @@ stoplist <- readLines(
 corpus <-tm_map(corpus, removeWords, stoplist)
 corpus <-tm_map(corpus, stripWhitespace)
 
+remove_char <- content_transformer(function(x, pattern) gsub(pattern, "", x))
+corpus <- tm_map(corpus, remove_char, intToUtf8(8722))
+corpus <- tm_map(corpus, intToUtf8(190))
+
+
+polish <- dictionary(lang="pl_PL")
+
+lemmatize <-function(text) {
+  simple_text <- str_trim(as.character(text))
+  parsed_text <- strsplit(simple_text, split = " ")
+  new_text_vec <- hunspell_stem(parsed_text[[1]], dict = polish)
+  for (i in 1:length(new_text_vec)) {
+    if (length(new_text_vec[[i]]) == 0) new_text_vec[i] <- parsed_text[[1]][i]
+    if (length(new_text_vec[[i]]) > 1) new_text_vec[i] <- new_text_vec[[1]][i]
+  }
+  new_text <- paste(new_text_vec, collapse = " ")
+  return(new_text)
+}
+corpus <- tm_map(corpus, content_transformer(lemmatize))
+
+cut_extensions <- function(document) {
+  meta(document, "id") <- gsub(pattern = "\\.txt$", replacement = "", meta(document, "id"))
+  return(document)
+}
+corpus <- tm_map(corpus, cut_extensions)
+
+preprocessedDir <- paste(
+  outputDir,
+  "Literatura - streszczenia - przetworzone",
+  sep = "\\"
+)
+dir.create(preprocessedDir)
+writeCorpus(corpus, path = preprocessedDir)
+
 writeLines(as.character(corpus[1]))
-writeLine(corpus[1]$content)
+writeLines(corpus[[1]]$content)
